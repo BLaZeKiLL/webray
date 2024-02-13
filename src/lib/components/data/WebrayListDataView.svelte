44e2186f419a85f22d6932e1e8e5e8e175b8a37e
Devashish Lal <laldevashish@gmail.com>
2024-02-12 19:44:29 -0800

    list styling
    Signed-off-by: Devashish Lal <laldevashish@gmail.com>

--- src/lib/components/data/WebrayListDataView.svelte
+++ src/lib/components/data/WebrayListDataView.svelte
@@ -11,33 +11,19 @@
 	$: store = scene.derived<any[]>(bind_path);
 </script>
 
-<!-- TODO: Need a virtual scroll view for performance reasons -->
-<!-- {#each $store as item (item.id)}
-	<WebrayDataView {data_type} bind_path={`${bind_path}[${item.id}]`}>
-		<span slot="header" class="flex flex-row items-center">
-			<h4 class="h4 text-surface-200">ID: {item.id}</h4>
-			<span class="grow"></span>
-			<button
-				on:click={() =>
-					WebrayEditor.invokeAction(ID.a_del_list_item, { bind_path: `${bind_path}[${item.id}]` })}
-			>
-				<WebrayIcon icon="i_delete_item" tooltip="Delete item" css="text-error-400" />
-			</button>
-		</span>
-	</WebrayDataView>
-{/each} -->
-
 <VirtualList items={$store} let:item>
-	<WebrayDataView {data_type} bind_path={`${bind_path}[${item.id}]`}>
-		<span slot="header" class="flex flex-row items-center">
-			<h4 class="h4 text-surface-200">ID: {item.id}</h4>
-			<span class="grow"></span>
-			<button
-				on:click={() =>
-					WebrayEditor.invokeAction(ID.a_del_list_item, { bind_path: `${bind_path}[${item.id}]` })}
-			>
-				<WebrayIcon icon="i_delete_item" tooltip="Delete item" css="text-error-400" />
-			</button>
-		</span>
-	</WebrayDataView>
+	<div class="mb-2">
+		<WebrayDataView {data_type} bind_path={`${bind_path}[${item.id}]`}>
+			<span slot="header" class="flex flex-row items-center">
+				<h4 class="h4 text-surface-200">ID: {item.id}</h4>
+				<span class="grow"></span>
+				<button
+					on:click={() =>
+						WebrayEditor.invokeAction(ID.a_del_list_item, { bind_path: `${bind_path}[${item.id}]` })}
+				>
+					<WebrayIcon icon="i_delete_item" tooltip="Delete item" css="text-error-400" />
+				</button>
+			</span>
+		</WebrayDataView>
+	</div>
 </VirtualList>
