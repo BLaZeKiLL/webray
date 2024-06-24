b9fa5c511ab75a8b823e431743106924ea31e4fc
Devashish Lal <laldevashish@gmail.com>
2024-06-24 15:24:05 -0700

    wasm log level changed
    Signed-off-by: Devashish Lal <laldevashish@gmail.com>

--- webray/src/lib.rs
+++ webray/src/lib.rs
@@ -18,7 +18,7 @@ pub fn initialize_kernel() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).unwrap();
+            console_log::init_with_level(log::Level::Info).unwrap();
         } else {
             env_logger::builder()
                 .filter_level(log::LevelFilter::Info)
